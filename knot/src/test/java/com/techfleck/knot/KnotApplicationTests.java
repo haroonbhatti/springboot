e19f4ef26ae3cc5d689089f6de35f778157b8bac
haroonbhatti <haroon.bhatti@outlook.com>
2022-06-04 22:55:22 -0400

    initial commit for knot app

---
+++ knot/src/test/java/com/techfleck/knot/KnotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techfleck.knot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
